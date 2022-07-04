089c761d2e1f5d274c676c82bb098a0493cb3c32
nhung <102678004+NhungNguyen1110@users.noreply.github.com>
2022-07-04 17:13:37 +0200

    Initial Commit

---
+++ Project4/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Project4
+//
+//  Created by Nhung on 04/07/2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
