85fb3b54a9f5d9554c0b217f66acf862b7700c05
nhung <102678004+NhungNguyen1110@users.noreply.github.com>
2022-07-05 13:54:13 +0200

    WIP

---
+++ Project4/Sources/Home/HomeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  HomeViewController.swift
+//  Project4
+//
+//  Created by Nhung on 05/07/2022.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+    // MARK: - Properties
+
+    // MARK: - IBOUtlets
+
+    // MARK: - View life cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do whatever important here for configuration
+    }
+
+    // MARK: - IBActions
+}
