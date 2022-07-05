93d5365f64404a01a8efd79f2d97dca8edec93bc
nhung <102678004+NhungNguyen1110@users.noreply.github.com>
2022-07-05 13:47:04 +0200

    Clean init project

---
+++ Project4/Sources/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Project4
+//
+//  Created by Nhung on 05/07/2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
