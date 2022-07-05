ce268c25a31005770b1de300e7f957f1c82d708a
nhung <102678004+NhungNguyen1110@users.noreply.github.com>
2022-07-05 13:43:49 +0200

    Recreate a iOS project

--- Project4/ViewController.swift
+++ Project4/ViewController.swift
@@ -2,23 +2,16 @@
 //  ViewController.swift
 //  Project4
 //
-//  Created by Nhung on 04/07/2022.
+//  Created by Nhung on 05/07/2022.
 //
 
-import Cocoa
+import UIKit
 
-class ViewController: NSViewController {
+class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
-    }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
     }
 
 
